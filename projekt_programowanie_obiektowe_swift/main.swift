557e1823b0bd10a6e93a5ad701d404d8654868cf
philljaysaw <philljaysaw@gmail.com>
2018-06-14 00:27:04 +0200

    Add main.swift

---
+++ projekt_programowanie_obiektowe_swift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  projekt_programowanie_obiektowe_swift
+//
+//  Created by phillip on 11/06/2018.
+//  Copyright © 2018 Phillip  Lawniczak. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+
+
